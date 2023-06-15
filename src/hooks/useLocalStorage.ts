831fadfdb10cd2d8c256c44facaecff77b6cc3ed
Rakotonirinaonjaniaina <hei.onjaniaina@gmail.com>
2023-06-15 10:38:40 +0300

    Add useLocalStorage hook for interacting with Local Storage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,19 @@
-const useLocalStorage = () => {
+import { useState, useEffect } from "react";
 
-}
+const useLocalStorage = (key, initialValue) => {
+  // Utilize useState to manage the value stored in Local Storage
+  const [value, setValue] = useState(() => {
+    // Check if the value already exists in Local Storage
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
 
-export {
-  useLocalStorage
-}
+  // Update Local Storage whenever the value changes
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export { useLocalStorage };
